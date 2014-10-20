3f49f7af7df11e8a7329790516ef515fd90f8a74
DarkLight <bentusy@tut.by>
2014-10-20 15:11:12 +0400

    Initial commit

---
+++ src/main/java/artcom/develop/java/clinic/ui/fx/MainWindow.java
@@ -0,0 +1,18 @@
+package artcom.develop.java.clinic.ui.fx;/**
+ * Created by DarkLight on 19.10.2014.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
